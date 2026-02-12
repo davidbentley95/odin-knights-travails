1a645d059f737fffcb3b9b7d9ef337fbea62791d
David Bentley <david.bentley95@gmail.com>
2026-02-12 07:07:16 -0800

    Implement shortest path algorithm

--- knightstravails.js
+++ knightstravails.js
@@ -27,6 +27,55 @@ function createNeighbors(coordinates) {
     return realNeighbors;
 }
 
-console.log(createNeighbors([4,3]));
+function knightMoves(start, end) {
 
-module.exports = { createNeighbors };
+    const queue = [start];
+    const visited = new Set();
+    const parentMap = new Map();
+    const results = [];
+
+    visited.add(start.join(','));
+
+    if(start.join(',') === end.join(',')) {
+        results.push(start);
+        return results;
+    }
+
+    while (queue.length !== 0) {
+
+        //get current target
+        let current = queue.shift();
+
+        //check if current target is end
+        //if it is, then reconstruct the parent path
+        if(current[0] === end[0] && current[1] === end[1]) {
+            results.unshift(current);
+            while(true) {
+                current = parentMap.get(current);
+                results.unshift(current);
+                if(current.join(',') === start.join(',')) {
+                    return results; 
+                }
+            }
+        }
+        
+        //create current target's neighbors
+        let neighbors = createNeighbors(current);
+        
+        //for each neighbor, check if they have been visited
+        //if not, add to queue and add parent mapping
+        //also add to visited now to stop shared spaces from being
+        //added on next queued item (ie 1,2 and 2,1 share 3,3 as a neighbor)
+        neighbors.forEach(item => {
+            const key = item.join(",");
+            if(!visited.has(key)) {
+                visited.add(key);
+                queue.push(item);
+                parentMap.set(item, current);
+            }
+        })
+    }
+    return results;
+}
+
+console.log(knightMoves([0,0], [7,7]));
